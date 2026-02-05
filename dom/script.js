58af653eeab5664d7ca889d4c9a5f0cc2847f3b2
Anshika Sengar <sengaranshika94@gmail.com>
2026-02-05 21:26:35 +0530

    dom

---
+++ dom/script.js
@@ -0,0 +1,8 @@
+let btn=document.querySelector("button")
+let box=document.querySelector("#box")
+btn.addEventListener("click",function(){
+    let c1= Math.floor(Math.random()*256)
+    let c2= Math.floor(Math.random()*256)
+    let c3= Math.floor(Math.random()*256)
+    box.style.backgroundColor=`rgb(${c1},${c2},${c3})`
+})
